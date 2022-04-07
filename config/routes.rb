e38008ad848529a2a0999779a34a4f98e4748144
chandana yanapu <chandana.yanapu@prasthana.com>
2022-04-07 12:36:21 +0530

    about index

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   #For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#home'
   get 'home', to: 'pages#home'
+  get 'index', to: 'pages#index'
+  get 'about', to: 'pages#about'
 end
